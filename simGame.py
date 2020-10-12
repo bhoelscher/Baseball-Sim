b9578e72bb69d58b65166ae6092888bf1a5c6543
Brett Hoelscher <bhoels202@gmail.com>
2020-10-11 20:50:06 -0500

    Added individual player stat tracking

--- simGame.py
+++ simGame.py
@@ -14,6 +14,25 @@ class Team:
         self.pitcher = pitcher
         self.next_hitter = 0
 
+class HittingGameStats:
+    at_bats = 0
+    hits = 0
+    runs = 0
+    rbis = 0
+    walks = 0
+    strikeouts = 0
+    doubles = 0
+    triples = 0
+    home_runs = 0
+
+class PitchingGameStats:
+    outs = 0
+    earned_runs = 0
+    hits = 0
+    strikeouts = 0
+    walks = 0
+    pitches = 0
+
 class Batter:
     def __init__(self, name, contact, power, speed, vision):
         self.name = name
@@ -21,6 +40,7 @@ class Batter:
         self.power = power
         self.speed = speed
         self.vision = vision
+        self.stats = HittingGameStats()
 
 class Pitcher:
     def __init__(self, name, control, velocity, movement, stamina):
@@ -29,6 +49,12 @@ class Pitcher:
         self.velocity = velocity
         self.movement = movement
         self.stamina = stamina
+        self.stats = PitchingGameStats()
+
+class GameResult:
+    def __init__(self, home_team, away_team):
+        self.home_team = home_team
+        self.away_team = away_team
 
 def simGame(away_team, home_team):
     away_score = 0
@@ -49,6 +75,8 @@ def simGame(away_team, home_team):
         away_hits += top_half_result.hits
         home_pitches += top_half_result.pitch_count
         away_team.next_hitter = top_half_result.next_hitter
+        away_team.batters = top_half_result.lineup
+        home_team.pitcher = top_half_result.pitcher
         #print(away_team + ": " + str(away_score) + ", " + home_team + ": " + str(home_score))
         if inning != 9 or home_score <= away_score:
             top = False
@@ -59,11 +87,15 @@ def simGame(away_team, home_team):
             home_hits += bottom_half_result.hits
             away_pitches += bottom_half_result.pitch_count
             home_team.next_hitter = bottom_half_result.next_hitter
+            home_team.batters = bottom_half_result.lineup
+            away_team.pitcher = bottom_half_result.pitcher
             #print(away_team + ": " + str(away_score) + ", " + home_team + ": " + str(home_score))
         inning += 1
     print("Final Score: " + away_team.name + ": " + str(away_score) + ", " + home_team.name + ": " + str(home_score))
     print(away_team.name + ": " + str(away_hits) + " hits and " + str(away_pitches) + " pitches")
     print(home_team.name + ": " + str(home_hits) + " hits and " + str(home_pitches) + " pitches")
+    result = GameResult(home_team, away_team)
+    return result
 
 batter1 = Batter("Ted", 70, 70, 70, 70)
 batter2 = Batter("Gregory", 80, 50, 80, 80)
@@ -75,13 +107,24 @@ batter7 = Batter("Mark", 85, 70, 65, 80)
 batter8 = Batter("Roberto", 60, 50, 70, 50)
 batter9 = Batter("Diego", 75, 40, 70, 80)
 
+batterh1 = Batter("Ted", 70, 70, 70, 70)
+batterh2 = Batter("Gregory", 80, 50, 80, 80)
+batterh3 = Batter("Melvin", 50, 90, 60, 70)
+batterh4 = Batter("Tim", 50, 50, 50, 50)
+batterh5 = Batter("Nick", 90, 90, 90, 90)
+batterh6 = Batter("Pat", 65, 80, 75, 80)
+batterh7 = Batter("Mark", 85, 70, 65, 80)
+batterh8 = Batter("Roberto", 60, 50, 70, 50)
+batterh9 = Batter("Diego", 75, 40, 70, 80)
+
 pitcher1 = Pitcher("Carlos", 90, 70, 40, 70)
 pitcher2 = Pitcher("Marco", 70, 80, 90, 70)
 
 lineup = [batter1, batter2, batter3, batter4, batter5, batter6, batter7, batter8, batter9]
+lineup2 = [batterh1, batterh2, batterh3, batterh4, batterh5, batterh6, batterh7, batterh8, batterh9]
 
 home_team = Team("Chicago", lineup, pitcher1)
-away_team = Team("New York", lineup, pitcher2)
+away_team = Team("New York", lineup2, pitcher2)
 
-simGame(home_team, away_team)
+x = simGame(home_team, away_team)
 
--- simHalfInning.py
+++ simHalfInning.py
@@ -4,13 +4,15 @@ import constants as cn
 import time
 
 class inningResult:
-    def __init__(self, runs_scored, pitch_count, hits, next_hitter):
+    def __init__(self, runs_scored, pitch_count, hits, next_hitter, lineup, pitcher):
         self.runs_scored = runs_scored
         self.pitch_count = pitch_count
         self.hits = hits
         self.next_hitter = next_hitter
+        self.lineup = lineup
+        self.pitcher = pitcher
 
-def simHalfInning(score, lineup, next_hitter, pitcher):
+def simHalfInning(score, lineup, current_hitter, pitcher):
     outs = 0
     pitch_count = 0
     runs_scored = 0
@@ -18,29 +20,38 @@ def simHalfInning(score, lineup, next_hitter, pitcher):
     baserunners = [False, False, False]
     while outs < 3 and not (score.inning >=9 and (score.home_score + runs_scored) > score.away_score and not score.top):
         runs_scored_batter = 0
-        batter = lineup[next_hitter]
+        batter = lineup[current_hitter]
         at_bat = simAtBat(batter, pitcher)
-        next_hitter += 1
-        if next_hitter == len(lineup):
-            next_hitter = 0
         pitch_count += at_bat.pitches
+        pitcher.stats.pitches += at_bat.pitches
         if at_bat.out:
             outs += 1
+            pitcher.stats.outs += 1
+            lineup[current_hitter].stats.at_bats += 1
+            if at_bat.out_type == cn.STRIKEOUT:
+                pitcher.stats.strikeouts += 1
+                lineup[current_hitter].stats.strikeouts += 1
             print(batter.name + ": " + str(at_bat.out_type) + ", " + str(outs) + " outs")
         elif at_bat.hit:
             hits += 1
+            pitcher.stats.hits += 1
+            lineup[current_hitter].stats.hits += 1
+            lineup[current_hitter].stats.at_bats += 1
             if at_bat.hit_type == cn.HOME_RUN:
+                lineup[current_hitter].stats.home_runs += 1
                 runs_scored_batter += 1
                 for runner in baserunners:
                     if runner:
                         runs_scored_batter += 1
                 baserunners = [False, False, False]
             elif at_bat.hit_type == cn.TRIPLE:
+                lineup[current_hitter].stats.triples += 1
                 for runner in baserunners:
                     if runner:
                         runs_scored_batter += 1
                 baserunners = [False, False, True]
             elif at_bat.hit_type == cn.DOUBLE:
+                lineup[current_hitter].stats.doubles += 1
                 if baserunners[cn.THIRD_BASE]:
                     runs_scored_batter += 1
                     baserunners[cn.THIRD_BASE] = False
@@ -93,6 +104,8 @@ def simHalfInning(score, lineup, next_hitter, pitcher):
                 baserunner_string += "third "
             print(batter.name + ": " + str(at_bat.hit_type) + ", " + str(runs_scored_batter) + " RBIs. " + baserunner_string)
         elif at_bat.walk:
+            lineup[current_hitter].stats.walks += 1
+            pitcher.stats.walks += 1
             if baserunners[cn.FIRST_BASE]:
                 if baserunners[cn.SECOND_BASE]:
                     if baserunners[cn.THIRD_BASE]:
@@ -108,8 +121,13 @@ def simHalfInning(score, lineup, next_hitter, pitcher):
             if baserunners[cn.THIRD_BASE]:
                 baserunner_string += "third "
             print(batter.name + ": " + "Walk "+ baserunner_string)
+        lineup[current_hitter].stats.rbis += runs_scored_batter
+        pitcher.stats.earned_runs += runs_scored_batter
         runs_scored += runs_scored_batter
+        current_hitter += 1
+        if current_hitter == len(lineup):
+            current_hitter = 0
         #time.sleep(1)
     print("Inning over, " + str(runs_scored) + " runs scored on " + str(hits) + " hits. " + str(pitch_count) + " pitches thrown")
-    result = inningResult(runs_scored, pitch_count, hits, next_hitter)
+    result = inningResult(runs_scored, pitch_count, hits, current_hitter, lineup, pitcher)
     return result
